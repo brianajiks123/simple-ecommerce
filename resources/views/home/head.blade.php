106c47613ce2b6464baa6cee4a7b9b0c04590a0f
Brian Aji Pamungkas <brianajiks123@gmail.com>
2024-09-30 11:08:03 +0700

    add bootstrap js file for user

--- resources/views/home/head.blade.php
+++ resources/views/home/head.blade.php
@@ -2,7 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <!-- Mobile Metas -->
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <!-- Site Metas -->
 <meta name="keywords" content="" />
 <meta name="description" content="" />
